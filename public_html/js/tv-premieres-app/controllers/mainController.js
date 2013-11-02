9d110fb79a106dc05be92dd1be6d3c7339780d0a
Erling G. M. Kristiansen <egkristi@gmail.com>
2013-11-02 21:19:20 +0100

    søk og filter

--- public_html/js/tv-premieres-app/controllers/mainController.js
+++ public_html/js/tv-premieres-app/controllers/mainController.js
@@ -1,9 +1,13 @@
-app.controller("mainController", function($scope, $http){
+app.controller("mainController", function($scope, $http) {
     $scope.apiKey = "3d2ed34ca956482e7dcf7bad53950175";
     $scope.results = [];
     $scope.filterText = null;
     $scope.availableGenres = [];
     $scope.genreFilter = null;
+    $scope.orderFields = ["Air Date", "Rating"];
+    $scope.orderDirections = ["Descending", "Ascending"];
+    $scope.orderField = "Air Date"; //Default order field
+    $scope.orderReverse = false;
     $scope.init = function() {
         //API requires a start date
         var today = new Date();
@@ -12,11 +16,11 @@ app.controller("mainController", function($scope, $http){
         $http.jsonp('http://api.trakt.tv/calendar/premieres.json/' + $scope.apiKey + '/' + apiDate + '/' + 30 + '/?callback=JSON_CALLBACK').success(function(data) {
             //As we are getting our data from an external source, we need to format the data so we can use it to our desired effect
             //For each day, get all the episodes
-            angular.forEach(data, function(value, index){
+            angular.forEach(data, function(value, index) {
                 //The API stores the full date separately from each episode. Save it so we can use it later
                 var date = value.date;
                 //For each episodes, add it to the results array
-                angular.forEach(value.episodes, function(tvshow, index){
+                angular.forEach(value.episodes, function(tvshow, index) {
                     //Create a date string from the timestamp so we can filter on it based on user text input
                     tvshow.date = date; //Attach the full date to each episode
                     $scope.results.push(tvshow);
@@ -37,7 +41,19 @@ app.controller("mainController", function($scope, $http){
             });
             //console.log($scope.results);
         }).error(function(error) {
- 
         });
+    };
+    $scope.setGenreFilter = function(genre) {
+        $scope.genreFilter = genre;
+    }
+    $scope.customOrder = function(tvshow) {
+        switch ($scope.orderField) {
+            case "Air Date":
+                return tvshow.episode.first_aired;
+                break;
+            case "Rating":
+                return tvshow.episode.ratings.percentage;
+                break;
+        }
     };
 });
