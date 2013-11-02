4f0680ea84185bb6123eac0990a90a49272cd9b1
Erling G. M. Kristiansen <egkristi@gmail.com>
2013-11-02 19:12:03 +0100

    AngularJS 2

---
+++ public_html/js/tv-premieres-app/controllers/filters.js
@@ -0,0 +1,17 @@
+app.filter('isGenre', function() {
+    return function(input, genre) {
+        if (typeof genre == 'undefined' || genre == null) {
+            return input;
+        } else {
+            var out = [];
+            for (var a = 0; a < input.length; a++){
+                for (var b = 0; b < input[a].show.genres.length; b++){
+                    if(input[a].show.genres[b] == genre) {
+                        out.push(input[a]);
+                    }
+                }
+            }
+            return out;
+        }
+    };
+});
